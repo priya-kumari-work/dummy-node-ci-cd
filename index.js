4111c7bbc7265f8599050dd66566e984ec5715df
Priya Kumari <priya@mobtions.com>
2025-12-16 11:24:09 +0530

    add dummy node app

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Dummy Node.js app deployed using GitHub Actions CI/CD 🚀");
+});
+
+app.get("/health", (req, res) => {
+  res.json({
+    status: "OK",
+    service: "dummy-node-ci-cd",
+    timestamp: new Date().toISOString(),
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
